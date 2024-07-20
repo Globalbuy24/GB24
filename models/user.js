7e641fda4563ad4fdfc49ed1f3c86e46d1b96621
dilan <nanadilan78@gmail.com>
2024-07-20 09:39:39 +0100

    GB-57

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const bcrypt=require('bcrypt')
 const UserSchema=new mongoose.Schema({
         token:{type:String},
         googleId:{type:String},
+        facebookId:{type:String},
         provider:{type:String},
         first_name:{
             type:String,
