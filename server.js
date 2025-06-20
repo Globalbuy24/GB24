ddf6d30f2e5072a3672df87da247123f330354ef
dilan <nanadilan78@gmail.com>
2025-06-20 09:44:34 +0100

    Fix on CORS

--- server.js
+++ server.js
@@ -13,26 +13,15 @@ db.once('open', () => console.log('Connected to database'));
 
 const app = express();
 
-// Define allowed origins
-const allowedOrigins = [
-    'https://ff-debug-service-frontend-pro-ygxkweukma-uc.a.run.app',
-    'http://localhost:3000', // Add localhost for local development
-    // Add other specific origins here if needed
-];
+// // Define allowed origins
+// const allowedOrigins = [
+//     'https://ff-debug-service-frontend-pro-ygxkweukma-uc.a.run.app',
+//     'http://localhost:3000', // Add localhost for local development
+//     // Add other specific origins here if needed
+// ];
 
 // Use CORS middleware
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true); // Allow requests with no origin
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }
-        return callback(new Error('Not allowed by CORS'));
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true // If you need cookies/auth headers
-}));
+app.use(cors());
 
 app.options('*',cors());
 
