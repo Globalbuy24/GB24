7fcdfaa56b2b755157d09900a5128ab63b9c58ce
Dilan Kenye <nanadilan78@gmail.com>
2025-07-16 18:37:32 +0100

    Admin auth

---
+++ middleware/currentAdminOnWeb.js
@@ -0,0 +1,22 @@
+// middleware/authenticate.js
+const jwt = require('jsonwebtoken');
+
+const authenticateAdmin = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { id: decoded.data }; 
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authenticateAdmin;
