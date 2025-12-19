c485dfd929c75d662760655b004d0526f6f05785
Nana-Dilan-kenye <nanadilan78@gmail.com>
2025-12-19 19:17:30 +0100

    Fixes on system default price and login plus admin endpoint for editing system defaults

--- models/system_default.js
+++ models/system_default.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const SystemDefault=new mongoose.Schema({
         service_fee:{
             percentage:{type:Number,default:0.00},
-            maxValue:{type:Number,default:0.00}
+            maxValue:{type:Number,default:25000}
         },
         delivery_fee:{air_freight:{type:Number,default:0.00},sea_freight:{type:Number,default:0.00}},
         loyalty_points:{referals:{type:Number,default:0.00},purchase:{type:Number,default:0.00}},
